a419ae405e4344906d7a149a8cb11c47be2e1854
Mujtaba <ahmad.mujtaba@spglobal.com>
2023-09-12 21:01:10 +0500

    Final changes like cross browser testing, video recording etc

--- pages/home_page.py
+++ pages/home_page.py
@@ -11,21 +11,18 @@ class HomePage(BasePage):
 
     @property
     def get_directions_button(self):
-        return self.page.locator(
-            ".directionsIcon")  # This selector is hypothetical; you need to find the actual selector from the Bing Maps page.
+        return self.page.locator(".directionsIcon")
 
     @property
     def search_bar(self):
-        return self.page.locator("#maps_sb")  # This selector is hypothetical.
+        return self.page.locator("#maps_sb")
 
-    def map_loaded(self):
-        # Check if some map element or attribute is present.
-        return self.page.locator("#map-element").is_visible()  # This selector is hypothetical.
+    # def map_loaded(self):
+    #     # Check if some map element or attribute is present.
+    #     return self.page.locator("#map-element").is_visible()  # This selector is hypothetical.
 
-    #def road_view_available(self):
-        #return self.page.locator("#road-view-option").is_visible()  # This selector is hypothetical.
+    # def road_view_available(self):
+    # return self.page.locator("#road-view-option").is_visible()
 
     def road_view_available(self):
         return self.page.locator('button img[alt="Road"]').is_visible()
-
-
--- tests/test_bing_maps.py
+++ tests/test_bing_maps.py
@@ -1,43 +1,75 @@
-# tests/test_bing_maps.py
-
-import unittest
+import pytest
+import os
+import glob
+import json
 from playwright.sync_api import sync_playwright
 from pages.home_page import HomePage
 
-
-class BingMapsTest(unittest.TestCase):
-
-    def setUp(self):
-        self.playwright = sync_playwright().start()
-
-        # Headless
-        self.browser = self.playwright.chromium.launch()
-
-        # Without Headless
-        # self.browser = self.playwright.chromium.launch(headless=False)
-        self.page = self.browser.new_page()
-        self.home = HomePage(self.page)
-        self.home.go_to("https://www.bing.com/maps/")
-
-    def test_page_title(self):
-        self.assertEqual(self.home.page_title, "Bing Maps - Directions, trip planning, traffic cameras & more")
-
-    def test_directions_button(self):
-        self.assertTrue(self.home.get_directions_button.is_visible())
-
-    def test_search_bar(self):
-        self.assertTrue(self.home.search_bar.is_visible())
-
-    def test_map_loads(self):
-        self.assertTrue(self.home.map_loaded())
-
-    def test_road_view(self):
-        self.assertTrue(self.home.road_view_available())
-
-    def tearDown(self):
-        self.browser.close()
-        self.playwright.stop()
+# Read the configuration from the JSON file
+config = json.load(open('C:\\Playwright_python\\config.json'))
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture(scope="function", params=config.keys())
+def browser_type(request):
+    with sync_playwright() as p:
+        browser = getattr(p, request.param).launch(**config[request.param])
+        yield browser
+        browser.close()
+
+
+# Fixture for video recording
+@pytest.fixture(scope="function")
+def page(browser_type, request):
+    video_path = f"C:\\Playwright_python\\videos\\{request.node.name}.webm"
+
+    # context = browser_type.new_context(
+    context = browser_type.new_context(
+        # record_video_dir='C:\\Playwright_python\\videos',  # Update the path where you want to store the videos
+        record_video_dir=os.path.dirname(video_path),
+        record_video_size={"width": 1280, "height": 720}
+    )
+    page = context.new_page()
+    yield page
+    page.close()
+    context.close()
+
+    # Rename the video file to match the test case name
+    # Assume that the generated video is the newest .webm file in the directory
+    list_of_files = glob.glob(os.path.dirname(video_path) + '/*.webm')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    os.rename(latest_file, video_path)
+
+
+@pytest.fixture(scope="function")
+def home_page(page):
+    home = HomePage(page)
+    home.go_to("https://www.bing.com/maps/")
+    return home
+
+
+def test_page_title(home_page):
+    assert home_page.page_title == "Bing Maps - Directions, trip planning, traffic cameras & more"
+
+
+def test_directions_button(home_page):
+    assert home_page.get_directions_button.is_visible
+
+
+def test_search_bar(home_page):
+    assert home_page.search_bar.is_visible
+
+
+# def test_map_loads(home_page):
+#     assert home_page.map_loaded
+
+
+def test_road_view(home_page):
+    assert home_page.road_view_available
+
+# def tearDown(self):
+#     self.browser.close()
+#     self.playwright.stop()
+#
+#
+# if __name__ == "__main__":
+#     unittest.main()
